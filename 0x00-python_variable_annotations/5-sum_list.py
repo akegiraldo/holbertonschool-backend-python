68580244fb37b3831de6bf73de5d9c536b6df8c9
akegiraldo <ake.giraldo@gmail.com>
2022-04-21 12:13:28 -0500

    [task 5] - Fix shebang

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
-from typing import List
-
+#!/usr/bin/env python3
 """Complex types - list of floats"""
+
+from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
