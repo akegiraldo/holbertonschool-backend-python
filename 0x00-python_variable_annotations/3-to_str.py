cc6d366222dc29ca27a17304a9f13fd73f54a215
akegiraldo <ake.giraldo@gmail.com>
2022-04-21 11:36:30 -0500

    [task 3] - Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""Basic annotations - to string"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function  that takes a
+    float n as argument and
+    returns the string
+    representation of the
+    float
+    """
+    return str(n)
