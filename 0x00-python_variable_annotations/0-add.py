a30d12e070abdadedf8ced1e05395831cc53174c
akegiraldo <ake.giraldo@gmail.com>
2022-04-21 11:08:19 -0500

    [task 0] - Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""Basic annotations - add"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Function that takes a float a and
+    a float b as arguments and returns
+    their sum as a float
+    """
+    return float(a + b)
